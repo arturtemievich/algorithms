12ed7cc79e347c32cfe677eec8a0db13cfa01b67
arturtemievich <arturtemievich@gmail.com>
2021-09-21 00:31:42 +0300

    add types of input and output

--- array.py
+++ array.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-def get_maximum_subarray_sum(array):
+def get_maximum_subarray_sum(array: List[int]) -> int:
     """Kadane’s algorithm is an iterative dynamic programming algorithm in which
     we search for a maximum sum contiguous subarray within a one-dimensional numeric array.
     Recurrence relation: array[i] = array[i] + array[i - 1] if array[i - 1] > 0 else array[i]
